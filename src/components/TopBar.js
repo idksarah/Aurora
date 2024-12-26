8fdd5b6c125559ada6e533347aa832e5d6efbb76
idksarah <sarah.p.ngai@gmail.com>
2024-12-26 14:58:37 -0800

    add router and example component

---
+++ src/components/TopBar.js
@@ -0,0 +1,11 @@
+// Example element
+
+function TopBar(){ //notice that first letter is capitalized and matches file name
+    return(
+        <div>
+            <p>Aurora</p>
+        </div>
+    )
+}
+
+export default TopBar;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+import TopBar from '../components/TopBar';
+
+function Home(){
+    return(
+        <>
+            <TopBar/>
+            <div className="content">
+                <p>whatever u want</p>
+            </div>
+        </>
+    )
+}
+
+export default Home;
