ced1ce4d751fbb1a13def0f00ab1224c092bcc91
idksarah <sarah.p.ngai@gmail.com>
2025-03-15 20:43:18 -0700

    content

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,4 +9,13 @@ function Card({name, img }) {
     );
 }
 
+export function PastEventCard({name, img }) {
+    return (
+        <div className="card pastEventCard">
+            <img className="img" src={img}></img>
+            <p className="text eventName">{name}</p>
+        </div>
+    );
+}
+
 export default Card;
